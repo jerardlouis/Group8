c0e3b1054df071d091846b554b5c335948052e0e
Jerard Dayanghirang <jerardlguevarra@gmail.com>
2021-04-09 19:43:20 +0000

    Added the new for you secion

--- react-starter/src/Main.js
+++ react-starter/src/Main.js
@@ -6,12 +6,30 @@ function Main() {
       <div className="Header">
       <h>Books</h>
       <img className="searchIcon" src="https://cdn3.iconfinder.com/data/icons/instagram-18/512/194_Instagram_Search_Sets-512.png" width="20" height="20"/>
+      <img className="bookMark" src="https://pics.freeicons.io/uploads/icons/png/15746672971543238863-512.png" width="20" height="20"/>
       <img className="icon" src= "https://static.thenounproject.com/png/630740-200.png" width="20" height="20"/>
       
       </div>
       <div className="HeaderButtons">
       <button class="button">Loan Book</button>
       <button class="button">Categories</button>
+      </div>
+      
+      <div className="NewForYou">
+      <h>New for you</h>
+      <p className="seeAll">See all (5)</p>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/3/3a/Book-icon-bible.png" height="60" width="60"/>
+      <p>
+      Bob replied to your message about his listing: Calc 2, used
+      </p>
+      <p className="daysAgo"> 1 day ago </p>
+      </div>
+      <div className="bookFeed">
+      <h> Today's picks </h>
+      <p> location </p>
+        <div className="feed">
+        
+        </div>
       </div>
     </div>
   );
