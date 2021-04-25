105c312857419a155f802c20d290bf62c735c023
Descartes Duval-Autus <ec2-user@ip-172-31-29-235.ec2.internal>
2021-04-25 21:02:45 +0000

    linting

--- src/Profile.js
+++ src/Profile.js
@@ -1,33 +1,41 @@
-import "./Main.css";
+import './Main.css';
+import PropTypes from 'prop-types';
+import React from 'react';
 
+function pass() {}
 function Profile(props) {
-  var bookIcon =
-    "https://upload.wikimedia.org/wikipedia/commons/3/3a/Book-icon-bible.png";
+  const bookIcon = 'https://upload.wikimedia.org/wikipedia/commons/3/3a/Book-icon-bible.png';
   return (
     <div className="Main">
       <div className="Header">
-        <h></h>
+        <h />
+        <div onClick={props.mainredirect} role="button" tabIndex={0} onKeyPress={props.mainredirect}>
+          <img
+            alt="backbutton"
+            className="backButton"
+            src="https://static.thenounproject.com/png/344330-200.png"
+            width="20"
+            height="20"
+          />
+        </div>
+        <div onClick={props.booksearch} role="button" tabIndex={0}>
+          <img
+            alt="search"
+            className="searchIcon"
+            src="https://cdn3.iconfinder.com/data/icons/instagram-18/512/194_Instagram_Search_Sets-512.png"
+            width="20"
+            height="20"
+          />
+        </div>
         <img
-          className="backButton"
-          onClick={props.mainredirect}
-          src="https://static.thenounproject.com/png/344330-200.png"
-          width="20"
-          height="20"
-        />
-        <img
-          className="searchIcon"
-          onClick={props.booksearch}
-          src="https://cdn3.iconfinder.com/data/icons/instagram-18/512/194_Instagram_Search_Sets-512.png"
-          width="20"
-          height="20"
-        />
-        <img
+          alt="save book"
           className="bookMark"
           src="https://pics.freeicons.io/uploads/icons/png/15746672971543238863-512.png"
           width="20"
           height="20"
         />
         <img
+          alt="profile icon"
           className="icon"
           src="https://static.thenounproject.com/png/630740-200.png"
           width="20"
@@ -39,6 +47,7 @@ function Profile(props) {
         <h>{props.username}</h>
         <p className="seeAll">
           <img
+            alt="leave a review"
             src="https://cdn2.iconfinder.com/data/icons/celebration-and-party-8/96/star-512.png"
             width="15"
             height="15"
@@ -46,6 +55,7 @@ function Profile(props) {
           5 stars
         </p>
         <img
+          alt="profile pic"
           src="https://static.thenounproject.com/png/630740-200.png"
           height="60"
           width="60"
@@ -56,13 +66,25 @@ function Profile(props) {
         <h> Books by USERNAME </h>
       </div>
       <div className="bookFeed">
-        <img className="img1" src={bookIcon} height="100" width="100" />
-        <img className="img2" src={bookIcon} height="100" width="100" />
-        <img className="img3" src={bookIcon} height="100" width="100" />
-        <img className="img4" src={bookIcon} height="100" width="100" />
+        <img alt="book1" className="img1" src={bookIcon} height="100" width="100" />
+        <img alt="book2" className="img2" src={bookIcon} height="100" width="100" />
+        <img alt="book3" className="img3" src={bookIcon} height="100" width="100" />
+        <img alt="book4" className="img4" src={bookIcon} height="100" width="100" />
       </div>
     </div>
   );
 }
+
+Profile.propTypes = {
+  booksearch: PropTypes.func,
+  mainredirect: PropTypes.func,
+  username: PropTypes.string,
+
+};
+Profile.defaultProps = {
+  booksearch: pass(),
+  mainredirect: pass(),
+  username: 'None',
+};
 
 export default Profile;
--- src/Search.js
+++ src/Search.js
@@ -1,41 +1,43 @@
-import "./App.css";
-import React, { useState } from "react";
-import Results from "./Results.js";
-const fetch = require("node-fetch");
+import './App.css';
+import React, { useState } from 'react';
+import Results from './Results';
+
+const fetch = require('node-fetch');
 
 function Search(props) {
-  //Renders the search page based on the categories from the database
+  // Renders the search page based on the categories from the database
   const [page, setpage] = useState(3);
   const [results, setresults] = useState([]);
-  const [category, setcategory] = useState("none");
-  function Clickhandler(category) {
-    console.log(category);
-    fetch("/booksearch/" + category, { method: "GET" })
+  const [category, setcategory] = useState('none');
+  function Clickhandler(categorys) {
+    console.log(categorys);
+    fetch(`/booksearch/${categorys}`, { method: 'GET' })
       .then((response) => response.json())
-      .then((data) => setresults(data["results"]));
-    //console.log(results);
-    setcategory(category);
+      .then((data) => setresults(data.results));
+    // console.log(results);
+    setcategory(categorys);
     setpage(4);
   }
-  if (page == 3) {
+  if (page === 3) {
     return (
       <div className="Main">
         <ul>
           {props.bookcategories.map((x) => (
-            <li onClick={() => Clickhandler(x)}>{x}</li>
+            <div onClick={() => Clickhandler(x)} role="button" tabIndex={0}><li>{x}</li></div>
           ))}
         </ul>
       </div>
     );
   }
-  if (page == 4)
+  if (page === 4) {
     return (
       <Results results={results} category={category} />
-      /*  <div className="Main">
+    /*  <div className="Main">
       <h1>{<ul>{results.map(x => <li>{x}</li>)}</ul>}</h1>
-      </div>*/
+      </div> */
     );
+  }
 }
 export default Search;
 
-//<ul>{results.map(x => <li>{x}</li>)}</ul>
+// <ul>{results.map(x => <li>{x}</li>)}</ul>
