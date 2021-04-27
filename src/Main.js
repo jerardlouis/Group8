e54021aa65b3e5b6f28e39333fc9772a96dc5936
mhk8 <ec2-user@ip-172-31-0-225.ec2.internal>
2021-04-27 20:45:53 +0000

    fixed username issue

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,45 @@
+import { useEffect, useState } from 'react';
 import './Main.css';
+
+const fetch = require("node-fetch")
 
 function Main(props) {
   var bookIcon= "https://upload.wikimedia.org/wikipedia/commons/3/3a/Book-icon-bible.png";
+   const [booksData, setBooksData] = useState([])
+
+    useEffect(() => {
+    fetch('http://localhost:8081/books', {
+      method: 'GET',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, PUT, DELETE, GET, OPTIONS',
+        'Access-Control-Request-Method': '*',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      },
+    }).then(response => response.json()).then(
+      response => {
+        setBooksData(response);
+      });
+  }, [])
+
+  let size_books = booksData.length;
+  const renderBooks = () => {
+    let book_html = []
+    console.log("called")
+    // Outer loop to create parent
+    // let size_books = booksData.length;
+    let size_books = 5;
+    while (size_books--) {
+      let book_one = <img className="img2" src={bookIcon} onClick={() => props.Clickhandler("Python for beginners")} height="100" width="100" />;
+      book_html.push(book_one);
+    }
+    return book_html
+  }
   return (
     <div className="Main">
       <div className="Header">
       <h>Books</h>
+      <img className="add" onClick = {props.addbook}src="https://img.icons8.com/metro/452/plus.png" width="20" height="20"/>
       <img className="searchIcon" onClick = {props.booksearch}src="https://cdn3.iconfinder.com/data/icons/instagram-18/512/194_Instagram_Search_Sets-512.png" width="20" height="20"/>
       <img className="bookMark" src="https://pics.freeicons.io/uploads/icons/png/15746672971543238863-512.png" width="20" height="20"/>
       <img className="icon" onClick = {props.profileredirect} src= "https://static.thenounproject.com/png/630740-200.png" width="20" height="20"/>
@@ -31,10 +65,11 @@ function Main(props) {
       </div>
       <div className="bookFeed">
       
-      <img className="img1" src={bookIcon} onClick={() => props.Clickhandler("Python for beginners")} height="100" width="100"/>
+      {/*<img className="img1" src={bookIcon} onClick={() => props.Clickhandler("Python for beginners")} height="100" width="100"/>
       <img className="img2" src={bookIcon} onClick={() => props.Clickhandler("Python for beginners")} height="100" width="100"/>
       <img className="img3" src={bookIcon} onClick={() => props.Clickhandler("Python for beginners")} height="100" width="100"/>
-      <img className="img4" src={bookIcon} onClick={() => props.Clickhandler("Python for beginners")} height="100" width="100"/>
+      <img className="img4" src={bookIcon} onClick={() => props.Clickhandler("Python for beginners")} height="100" width="100"/>*/}
+        {renderBooks()}
       </div>
     </div>
   );
